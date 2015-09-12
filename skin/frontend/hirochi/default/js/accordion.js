8903731f87b4a9179d670412bc5d720d37ff5669
megumipan <m.sato@pikul.jp>
2015-09-12 14:41:38 +0900

    プロトタイプjsとの衝突を回避

--- skin/frontend/hirochi/default/js/accordion.js
+++ skin/frontend/hirochi/default/js/accordion.js
@@ -1,13 +1,13 @@
-$(function(){
-$(".box ul").hide();
-$(".box p").click(function(){
-$(this).next("ul").slideToggle();
-    $(this).children("span").toggleClass("open");
-});
-$(".nav ul").hide();
-$(".nav p").click(function(){
-$(this).next("ul").slideToggle();
-    $(this).children("span").toggleClass("open");
-});
- 
+jQuery.noConflict();
+jQuery(document).ready(function($){
+    $(".box ul").hide();
+    $(".box p").click(function(){
+    $(this).next("ul").slideToggle();
+        $(this).children("span").toggleClass("open");
+    });
+    $(".nav ul").hide();
+    $(".nav p").click(function(){
+    $(this).next("ul").slideToggle();
+        $(this).children("span").toggleClass("open");
+    });
 });
