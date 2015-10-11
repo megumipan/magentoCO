21bd3b9fa4c02ea232f5a78363cb57ac0d410620
Kimura <kimura@pikul.jp>
2015-10-11 15:03:24 +0900

    No.12対応

--- app/code/core/Mage/Wishlist/controllers/IndexController.php
+++ app/code/core/Mage/Wishlist/controllers/IndexController.php
@@ -476,6 +476,7 @@ class Mage_Wishlist_IndexController extends Mage_Wishlist_Controller_Abstract
 
         /* @var $item Mage_Wishlist_Model_Item */
         $item = Mage::getModel('wishlist/item')->load($itemId);
+
         if (!$item->getId()) {
             return $this->_redirect('*/*');
         }
@@ -490,12 +491,17 @@ class Mage_Wishlist_IndexController extends Mage_Wishlist_Controller_Abstract
             if (isset($qty[$itemId])) {
                 $qty = $qty[$itemId];
             } else {
-                $qty = 1;
+                // If qty is zero, rerutn error.
+                //$qty = 1;
+                $qty = 0;
             }
         }
-        $qty = $this->_processLocalizedQty($qty);
-        if ($qty) {
-            $item->setQty($qty);
+
+        if ($qty > 0) {
+          $qty = $this->_processLocalizedQty($qty);
+          if ($qty) {
+              $item->setQty($qty);
+          }
         }
 
         /* @var $session Mage_Wishlist_Model_Session */
@@ -504,48 +510,52 @@ class Mage_Wishlist_IndexController extends Mage_Wishlist_Controller_Abstract
 
         $redirectUrl = Mage::getUrl('*/*');
 
-        try {
-            $options = Mage::getModel('wishlist/item_option')->getCollection()
-                    ->addItemFilter(array($itemId));
-            $item->setOptions($options->getOptionsByItem($itemId));
+        if ($qty > 0) {
+          try {
+              $options = Mage::getModel('wishlist/item_option')->getCollection()
+                      ->addItemFilter(array($itemId));
+              $item->setOptions($options->getOptionsByItem($itemId));
 
-            $buyRequest = Mage::helper('catalog/product')->addParamsToBuyRequest(
-                $this->getRequest()->getParams(),
-                array('current_config' => $item->getBuyRequest())
-            );
+              $buyRequest = Mage::helper('catalog/product')->addParamsToBuyRequest(
+                  $this->getRequest()->getParams(),
+                  array('current_config' => $item->getBuyRequest())
+              );
 
-            $item->mergeBuyRequest($buyRequest);
-            if ($item->addToCart($cart, true)) {
-                $cart->save()->getQuote()->collectTotals();
-            }
+              $item->mergeBuyRequest($buyRequest);
+              if ($item->addToCart($cart, true)) {
+                  $cart->save()->getQuote()->collectTotals();
+              }
 
-            $wishlist->save();
-            Mage::helper('wishlist')->calculate();
+              $wishlist->save();
+              Mage::helper('wishlist')->calculate();
 
-            if (Mage::helper('checkout/cart')->getShouldRedirectToCart()) {
-                $redirectUrl = Mage::helper('checkout/cart')->getCartUrl();
-            }
-            Mage::helper('wishlist')->calculate();
+              if (Mage::helper('checkout/cart')->getShouldRedirectToCart()) {
+                  $redirectUrl = Mage::helper('checkout/cart')->getCartUrl();
+              }
+              Mage::helper('wishlist')->calculate();
 
-            $product = Mage::getModel('catalog/product')
-                ->setStoreId(Mage::app()->getStore()->getId())
-                ->load($item->getProductId());
-            $productName = Mage::helper('core')->escapeHtml($product->getName());
-            $message = $this->__('%s was added to your shopping cart.', $productName);
-            Mage::getSingleton('catalog/session')->addSuccess($message);
-        } catch (Mage_Core_Exception $e) {
-            if ($e->getCode() == Mage_Wishlist_Model_Item::EXCEPTION_CODE_NOT_SALABLE) {
-                $session->addError($this->__('This product(s) is currently out of stock'));
-            } else if ($e->getCode() == Mage_Wishlist_Model_Item::EXCEPTION_CODE_HAS_REQUIRED_OPTIONS) {
-                Mage::getSingleton('catalog/session')->addNotice($e->getMessage());
-                $redirectUrl = Mage::getUrl('*/*/configure/', array('id' => $item->getId()));
-            } else {
-                Mage::getSingleton('catalog/session')->addNotice($e->getMessage());
-                $redirectUrl = Mage::getUrl('*/*/configure/', array('id' => $item->getId()));
-            }
-        } catch (Exception $e) {
-            Mage::logException($e);
-            $session->addException($e, $this->__('Cannot add item to shopping cart'));
+              $product = Mage::getModel('catalog/product')
+                  ->setStoreId(Mage::app()->getStore()->getId())
+                  ->load($item->getProductId());
+              $productName = Mage::helper('core')->escapeHtml($product->getName());
+              $message = $this->__('%s was added to your shopping cart.', $productName);
+              Mage::getSingleton('catalog/session')->addSuccess($message);
+          } catch (Mage_Core_Exception $e) {
+              if ($e->getCode() == Mage_Wishlist_Model_Item::EXCEPTION_CODE_NOT_SALABLE) {
+                  $session->addError($this->__('This product(s) is currently out of stock'));
+              } else if ($e->getCode() == Mage_Wishlist_Model_Item::EXCEPTION_CODE_HAS_REQUIRED_OPTIONS) {
+                  Mage::getSingleton('catalog/session')->addNotice($e->getMessage());
+                  $redirectUrl = Mage::getUrl('*/*/configure/', array('id' => $item->getId()));
+              } else {
+                  Mage::getSingleton('catalog/session')->addNotice($e->getMessage());
+                  $redirectUrl = Mage::getUrl('*/*/configure/', array('id' => $item->getId()));
+              }
+          } catch (Exception $e) {
+              Mage::logException($e);
+              $session->addException($e, $this->__('Cannot add item to shopping cart'));
+          }
+        } else {
+          $session->addError($this->__('Unable to add the following product(s) to shopping cart: %s.',$item->getProduct()->getName()));
         }
 
         Mage::helper('wishlist')->calculate();
